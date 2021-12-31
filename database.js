bcf85db70f9f0328cc9ac2810d74caa71093e863
apoorva0107 <apoorva0107@gmail.com>
2021-12-31 22:27:36 +0530

    database connected

---
+++ database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: "./.env" });
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+  );
+  
+  // Database connection
+  const mongo = mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log(`DB connection successful!`));
