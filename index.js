64ad90a0a142d47f86ed1e281d7600e5cc8ee9f8
apoorva0107 <apoorva0107@gmail.com>
2022-01-01 00:38:14 +0530

    Updated the file system

--- index.js
+++ index.js
@@ -1,37 +1,4 @@
 require('./database');
-const Question = require("./model/questions");
-const {getQuestions, retrieveQuesData} = require('./quesList');
-const taskArray = [];
+const {webCrawler} = require('./scripts/data-crawler');
 
-const storeQueue = (arr) => {
-    taskArray.push(...arr);
-};
-
-const crawlData = async () => {
-  const url = taskArray.shift();
-  console.log("URL: ", url);
-  const currentQuestion = await Question.findOne({ url });
-
-  if (!currentQuestion) {
-    await retrieveQuesData(url, num, storeQueue);
-  } else {
-    currentQuestion.totalReferenceCount += 1;
-    await currentQuestion.save();
-    await getQuestions(num + 1, storeQueue);
-    num += 1;
-  }
-};
-
-let num = 1;
-const crawler = async () => {
-  await getQuestions(num, storeQueue);
-  while (taskArray.length > 0) {
-    await crawlData();
-    await crawlData();
-    await crawlData();
-    await crawlData();
-    await crawlData();
-  }
-};
-
-crawler();
+webCrawler();
